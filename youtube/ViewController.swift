5cef3ed3c60e69020b64117060e2c54b53e20d6f
Alejandro Peña <alejandro@MacBook-Pro-de-Alejandro.local>
2020-06-16 13:26:52 -0500

    Initial xCode project creation

---
+++ youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by Alejandro Peña on 16/06/20.
+//  Copyright © 2020 Alejandro Peña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
